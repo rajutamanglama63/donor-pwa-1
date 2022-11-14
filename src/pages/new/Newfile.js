830c27ef3e4ff43c56a128e8d5cdf2fd0767ee4b
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-11-14 13:55:13 +0545

    chore: initialized project

---
+++ src/pages/new/Newfile.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { Button } from "@mui/material";
+import { ModuleContext, useModuleContext } from "./context";
+
+const Newfile = (props) => {
+  // const v = useContext(ModuleContext);
+  // second test
+  const { changeSth, getListData, list } = useModuleContext();
+
+  console.log("list", list);
+
+  useEffect(() => {
+    getListData();
+    //   return () => {
+
+    //   }
+  }, []);
+
+  return (
+    <div>
+      <Button onClick={changeSth}>New Button</Button>
+    </div>
+  );
+};
+
+Newfile.propTypes = {};
+
+export default Newfile;
